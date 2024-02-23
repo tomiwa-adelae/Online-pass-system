ab5c34d9fd1131a93b6ae15a15f7882dbc79f188
Tomiwa Adelae <thetommedia@gmail.com>
2024-02-23 01:51:29 +0100

    Created the landing page

---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Montagu_Slab } from "next/font/google";
+
+const montaguSlab = Montagu_Slab({
+	weight: ["400"],
+	subsets: ["latin"],
+});
+
+const Home = () => {
+	return (
+		<div className="homepage">
+			<header>
+				<div className="logo">
+					<img
+						src="./passify-logo-white.png"
+						alt="Passify with the P as the logo"
+					/>
+				</div>
+				<a href="./login" className="btn btn-white">
+					Join
+				</a>
+			</header>
+			<main>
+				<div className="container">
+					<h4>Vacation is here!</h4>
+					<h1 className={montaguSlab.className}>Time to travel</h1>
+					<p className="intro">
+						Welcome to Passify, where hassle-free gate pass
+						management is our top priority! Say goodbye to
+						cumbersome processes and endless paperwork. Streamline
+						your operations and enjoy a seamless experience with our
+						innovative solutions. Discover the future of gate pass
+						management today!
+					</p>
+					<a href="./login" className="btn btn-white">
+						Join
+					</a>
+				</div>
+			</main>
+		</div>
+	);
+};
+
+export default Home;

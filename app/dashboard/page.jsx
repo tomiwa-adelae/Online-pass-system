8cd07467da330f3f8cdd5359dea08b7965877932
Tomiwa Adelae <thetommedia@gmail.com>
2024-03-06 17:11:31 +0100

    Started the Dashboard page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
 import React from "react";
 
 const page = () => {
-	return <div className="dashboardpage">page</div>;
+	return (
+		<div className="dashboardpage">
+			<Header />
+		</div>
+	);
 };
 
 export default page;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-	return <header>Header</header>;
+	return (
+		<header>
+			<div className="logo">
+				<img
+					src="./passify-logo.png"
+					alt="Passify with the P as the logo"
+				/>
+			</div>
+			<Link href="./auth" className="btn btn-white">
+				Join
+			</Link>
+		</header>
+	);
 };
 
 export default Header;
